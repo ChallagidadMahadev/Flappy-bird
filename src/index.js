59caac54a53cfe5bbf9031bfaa9ff40dba018acc
M A D D Y <maddy@MADDYs-Air.lan>
2024-04-25 00:30:52 +0530

    pipe movement added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 200 },
+      // gravity: { y: 200 },
       debug: true,
     },
   },
@@ -20,22 +20,53 @@ const config = {
 };
 
 const VELOCITY = 200;
+
+const PIPE_RENDER = 4;
+const pipeVerticleDistanceRange = [150, 250];
+let upperPipe = null
+let lowerPipe = null;
+
+let pipeHorizontalDistance = 0;
+
 let flapVelocity = 250;
+
 const initialBirdPos = {
   x: config.width / 10,
   y: config.height / 2,
 };
 let bird = null;
+
 function preload() {
   this.load.image("sky", "assets/sky.png");
   this.load.image("bird", "assets/bird.png");
+  this.load.image("pipe", "assets/pipe.png");
 }
 function create() {
   this.add.image(0, 0, "sky").setOrigin(0, 0);
   bird = this.physics.add
     .sprite(initialBirdPos.x, initialBirdPos.y, "bird")
     .setOrigin(0, 0);
-  // bird.body.velocity.x = VELOCITY;
+  bird.body.gravity.y = 400;
+
+
+  for (let i = 0; i < PIPE_RENDER; i++) {
+    pipeHorizontalDistance += 400;
+
+    let pipeVerticleDistance = Phaser.Math.Between(
+      ...pipeVerticleDistanceRange
+    );
+    let pipeVerticlePosition = Phaser.Math.Between(
+      20,
+      config.height - 20 - pipeVerticleDistance
+    );
+
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance,pipeVerticlePosition,'pipe').setOrigin(0,1)
+    lowerPipe = this.physics.add
+      .sprite(upperPipe.x, upperPipe.y+pipeVerticleDistance, "pipe")
+      .setOrigin(0, 0);
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
+  }
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown-SPACE", flap);
 }
